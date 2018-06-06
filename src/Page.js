486128ad7b45f095579582ebf2b247c589a5719d
PierreTurnbull <pierre.turnbull42@gmail.com>
2018-06-06 18:26:33 +0200

    build is deployed

--- src/Page.js
+++ src/Page.js
@@ -80,7 +80,6 @@ class Page extends Component {
                                 />
                             </a>
                         );
-                        return "";
                     })
                 }
             </div>
